f3ebcc25bcf8914e7eaf319090a1753aa58f6789
mbzheng <75959699+mbzheng@users.noreply.github.com>
2020-12-14 20:45:32 +0800

    Add files via upload

---
+++ cloud-api-common/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -0,0 +1,49 @@
+package com.atguigu.springcloud.entities;
+
+public class CommonResult<T> {
+    private Integer code;
+    private String  message;
+    private Object  data;
+
+    public CommonResult() {
+    }
+
+    public CommonResult(Integer code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "CommonResult{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
