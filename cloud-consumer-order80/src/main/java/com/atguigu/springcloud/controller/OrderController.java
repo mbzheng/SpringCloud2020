5910219df4af13b0c714222bb04a6b8695401c01
ISOFTSTONE\mbzheng <mbzheng@isoftstone.com>
2020-12-16 16:07:17 +0800

    增加订单信息查询接口

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,12 +2,15 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -21,6 +24,8 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("consumer/payment/create")
     public CommonResult create(Payment payment){
@@ -33,4 +38,20 @@ public class OrderController {
         return  restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
+
+    @GetMapping(value = "consumer/discover")
+    public Object discover(){
+        List<String> services= discoveryClient.getServices();
+        for (String s:services){
+            System.out.println(s);
+        }
+
+        List<ServiceInstance> instances=discoveryClient.getInstances("order-consumer-order80");
+
+        for (ServiceInstance s:instances){
+            System.out.println(s.getServiceId()+"\t"+s.getHost()+"\t"+s.getPort()+"\t"+s.getUri());
+        }
+
+        return this.discoveryClient;
+    }
 }
