c1cc49b9fb464fb279d14de1d3184a7ec8d57f41
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-08 14:00:33 +0800

    解决客户端动态刷新配置问题

--- cloud-config-client-3355/src/main/java/com/atgui/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/atgui/springcloud/ConfigClientMain3355.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class ConfigClientMain3355 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientMain3355.class,args);
