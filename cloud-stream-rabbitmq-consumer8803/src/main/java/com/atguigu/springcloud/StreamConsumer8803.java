b2f3e375d771060bdbf860a738b38f1f167d2e81
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-12 01:27:45 +0800

    增加消费者8803模块

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamConsumer8803.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumer8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumer8803.class,args);
+        System.out.println("StreamConsumer8803");
+    }
+}
