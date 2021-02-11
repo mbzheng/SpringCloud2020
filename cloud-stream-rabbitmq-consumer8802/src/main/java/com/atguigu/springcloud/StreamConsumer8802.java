7cfa7d3465cdf5c31f5ddb0bb15d1b63a48c4f2f
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-12 01:22:07 +0800

    增加消息驱动消费者8802模块

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamConsumer8802.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumer8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumer8802.class,args);
+        System.out.println("StreamConsumer8802");
+    }
+}
