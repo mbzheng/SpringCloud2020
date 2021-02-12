9a153d4e50172305a9273244d479332435b32008
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-12 23:14:54 +0800

    增加nacos分布式配置统一管理模块，利用namespace，group，dataid分层

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfig3377.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfig3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfig3377.class,args);
+        System.out.println("NacosConfig3377");
+    }
+}
