ee1913298cc9cd2145847ea25beaa5e0f571f436
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-16 23:44:50 +0800

    测试服务降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -4,19 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
-    public String testA()
-    {
+    public String testA(){
+        System.out.println(Thread.currentThread().getName()+"\t"+"......testA");
         return "------testA";
     }
 
     @GetMapping("/testB")
-    public String testB()
-    {
+    public String testB() {
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");
+//        log.info("testD 异常比例");
+//        int age = 10/0;
+        return "------testD";
     }
 
 
