e01612a58845076f7bb3f68732934b147c522f82
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-16 23:55:41 +0800

    测试异常数测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -22,13 +22,18 @@ public class FlowLimitController {
 
     @GetMapping("/testD")
     public String testD() {
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-        log.info("testD 测试RT");
-//        log.info("testD 异常比例");
-//        int age = 10/0;
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age = 10/0;
         return "------testD";
     }
 
-
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
 
 }
