a9429a28117fcd9e4f7f74d892444b1638bb4694
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-17 14:41:01 +0800

    add globa fallback

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.alibaba.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.alibaba.myhandler.CustomerBlockHandler;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +27,12 @@ public class RateLimitController
     }
 
 
-//    @GetMapping("/rateLimit/customerBlockHandler")
-//    @SentinelResource(value = "customerBlockHandler",
-//            blockHandlerClass = CustomerBlockHandler.class,
-//            blockHandler = "handlerException2")
-//    public CommonResult customerBlockHandler()
-//    {
-//        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
-//    }
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler()
+    {
+        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
+    }
 }
