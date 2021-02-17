c87b190285c8f817c70d5725ad175e407a5e7db7
DESKTOP-VUFBR9S\19166 <1916697473@qq.com>
2021-02-17 14:30:18 +0800

    使用SentinelResource注解并测试

--- cloud-api-common/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-api-common/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -13,6 +13,11 @@ public class CommonResult<T> {
         this.message = message;
         this.data = data;
     }
+    public CommonResult(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+
+    }
 
     @Override
     public String toString() {
